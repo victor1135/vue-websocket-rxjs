166804a89227001e155ff770cbad2f6f86f8d834
felix <ac94865358@gmail.com>
2019-01-05 00:32:31 +0800

    connect websocket  by rxjs

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import VueRx from 'vue-rx';
+
 
 Vue.config.productionTip = false;
-
+Vue.use(VueRx);
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: (h) => h(App)
 }).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button @click="requestKenHead">Shake it!!!</button>
+    <img
+      src="../assets/logo.png"
+      :style="item.style"
+      v-for="(item, key) in images"
+      v-if="item.disable"
+    >
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import { of, merge, timer } from "rxjs";
+import { webSocket, WebSocketSubject } from "rxjs/webSocket";
+import { groupBy, mergeMap, takeUntil, finalize, tap } from "rxjs/operators";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+@Component
+export default class Home extends Vue {
+  socket$: WebSocketSubject<string> | any = new WebSocketSubject(
+    "ws://localhost:8080"
+  );
+  images: any = {};
+
+  mounted() {
+    const alphaHead = this.socket$.pipe(
+      groupBy((data: any) => data.uid),
+      mergeMap((singleAlphaStream: any) =>
+        singleAlphaStream.pipe(
+          takeUntil(timer(3000)),
+          finalize(() => {
+            
+            const image = this.images[singleAlphaStream.key].disable = false;
+            console.log(image);
+            
+          })
+        )
+      )
+    );
+
+    alphaHead.subscribe(
+      (data: any) => {
+        const { x, y, uid } = data;
+        this.images[data.uid] = {
+          uid: data.uid,
+          disable: true,
+          style: {
+            position: "absolute",
+            left: x + "px",
+            top: y + "px",
+          }
+        };
+        const obj = Object.assign({}, this.images);
+        this.$set(this.images, 'images', obj)
+      },
+      (err: any) => console.error(err),
+      () => console.warn("Completed!")
+    );
+    this.requestKenHead();
+  }
+
+  requestKenHead() {
+    this.socket$.next({
+      event: "events",
+      data: { type: "REQUEST_KEN_HEAD" }
+    });
+  }
+}
 </script>
